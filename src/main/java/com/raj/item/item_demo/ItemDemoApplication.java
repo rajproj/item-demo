b1d9e44c04cf8d21e97e4c13dfc2bbb33f01f073
rajproj <crhkprj@gmail.com>
2024-06-08 21:22:25 +0530

    Initial commit

---
+++ src/main/java/com/raj/item/item_demo/ItemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.raj.item.item_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raj/item/item_demo/ItemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.item.item_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
